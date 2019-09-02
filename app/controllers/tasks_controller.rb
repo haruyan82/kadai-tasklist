2e4957e4d28fe68282c8a118d15c584178a40db3
yohiharu.yanagi <haruyan82@gmail.com>
2019-09-02 11:52:20 +0000

    CRUD更新

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,7 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:update,:destroy]
+    
     def index
         @tasks =Task.all
     end
