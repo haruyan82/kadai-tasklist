26453094d1edccde4a4d4ee54fd614e9b72ec5a1
yohiharu.yanagi <haruyan82@gmail.com>
2019-09-05 14:07:42 +0000

    CRUD操作制限

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,9 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:update,:destroy]
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     def index
-        @tasks = @current_user.tasks
+        @tasks = current_user.tasks.all
     end
     
     def show
@@ -57,10 +57,10 @@ class TasksController < ApplicationController
      params.require(:task).permit(:content, :status)
     end
     
-    def currect_user
+    def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
       unless @task
-      　redirect_to tasks_url
+        redirect_to root_url
       end
     end
 end
